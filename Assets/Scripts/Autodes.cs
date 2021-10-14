1c438049532122518a600c00c34bddb3c3f5c0b9
9503chl <9503chl@naver.com>
2021-10-14 15:49:52 +0900

    '21.10.14'

---
+++ Assets/Scripts/Autodes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Autodes : MonoBehaviour
+{
+    public float time = 3.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, time);
+    }
+}
---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    void Start()
+    {
+        var velo = speed * transform.forward;
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(speed * transform.forward, ForceMode.VelocityChange);
+    }
+}
