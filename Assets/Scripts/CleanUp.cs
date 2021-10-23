71ef1ba999def01e284e0a34c2db67194802780d
9503chl <9503chl@naver.com>
2021-10-23 23:33:52 +0900

    '21.10.23.2'

--- Assets/Scripts/CleanUp.cs
+++ Assets/Scripts/CleanUp.cs
@@ -8,35 +8,34 @@ public class CleanUp : MonoBehaviour
     public Transform Target2;
     public Transform Target3;
     public Transform Target4;
-
-    void Update()
+    void FixedUpdate()
     {
-        if (GameObject.Find("BreakWall").GetComponent<DesPin>().isFirst)
+        //if (GameObject.Find("BreakWall").GetComponent<DesPin>().isFirst)
         {
             Invoke("FirstM", 0.5f);
-            Invoke("SecondM", 1.5f);
-            Invoke("ThirdM", 2.5f);
-            Invoke("FourthM", 3.5f);
+            if (gameObject.transform.position.y - Target1.position.y == 0) SecondM();
+            if (gameObject.transform.position.z - Target2.position.z == 0) ThirdM();
+            if (gameObject.transform.position.y - Target3.position.y == 0) FourthM();
         }
     }
     void FirstM()
     {
-        transform.position = Vector3.Lerp(transform.position, Target1.position, 10.0f * Time.deltaTime);
+        transform.position = Vector3.Slerp(transform.position, Target1.position, 10.0f *Time.deltaTime);
         
     }
     void SecondM()
     {
-        transform.position = Vector3.Lerp(transform.position, Target2.position, 10.0f * Time.deltaTime);
+        transform.position = Vector3.Slerp(transform.position, Target2.position, 10.0f * Time.deltaTime);
         
     }
     void ThirdM()
     {
-        transform.position = Vector3.Lerp(transform.position, Target3.position, 10.0f *  Time.deltaTime);
+        transform.position = Vector3.Slerp(transform.position, Target3.position, 10.0f *Time.deltaTime);
         
     }
     void FourthM()
     {
-        transform.position = Vector3.Lerp(transform.position, Target4.position, 10.0f * Time.deltaTime);
+        transform.position = Vector3.Slerp(transform.position, Target4.position, 10.0f* Time.deltaTime);
         GameObject.Find("BreakWall").GetComponent<DesPin>().isFirst = false;
     }
 }
