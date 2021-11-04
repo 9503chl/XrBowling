8e49525f474c39091b4f9e1649c76cc16b132a9c
9503chl <9503chl@naver.com>
2021-11-05 00:50:41 +0900

    '21.11.05'

--- Assets/Scripts/CharMove.cs
+++ Assets/Scripts/CharMove.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class CharMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (OVRInput.Get(OVRInput.Touch.PrimaryThumbstick)) {
+
+        }
     }
- 
 }
