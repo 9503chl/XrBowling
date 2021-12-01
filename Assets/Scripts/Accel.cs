824018f2dffdb03c3c0515098533444f2c92fdee
9503chl <9503chl@naver.com>
2021-12-01 23:02:57 +0900

    '21.12.01.2'

--- Assets/Scripts/Accel.cs
+++ Assets/Scripts/Accel.cs
@@ -6,7 +6,8 @@ public class Accel : MonoBehaviour
 {
     private Transform tr;
     public bool isMove = false;
-    
+    public bool isLeft = false;
+    public bool isRight = false;
     void Start()
     {
         tr = GetComponent<Transform>();
@@ -16,8 +17,29 @@ public class Accel : MonoBehaviour
     {
         if (isMove)
         {
-            tr.transform.Translate(Vector3.forward * Time.deltaTime * 5.0f, Space.World);
-            tr.transform.Rotate(10, 0, 0);
+            if (isLeft)
+            {
+                tr.transform.Translate(new Vector3(0, -0.1f, 1) * Time.deltaTime * 5.0f, Space.World);
+                tr.transform.Rotate(10, -10f, 0);
+            }
+            else if (isRight)
+            {
+                tr.transform.Translate(new Vector3(0,0.1f,1) * Time.deltaTime * 5.0f, Space.World);
+                tr.transform.Rotate(10, 10f, 0);
+            }
+            else
+            {
+                tr.transform.Translate(Vector3.forward * Time.deltaTime * 5.0f, Space.World);
+                tr.transform.Rotate(10, 0, 0);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A)) //¾È ¸ÔÈù´Ù
+        {
+            isLeft = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D)) // ¾È¸ÔÈù´Ù.
+        {
+            isRight = true;
         }
     }
 
