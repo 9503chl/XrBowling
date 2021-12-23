135cb90234080eb5230b2803307bc7c3ec6dc545
9503chl <9503chl@naver.com>
2021-12-23 14:38:29 +0900

    '21.12.23'

--- Assets/Scripts/UIText.cs
+++ Assets/Scripts/UIText.cs
@@ -2,29 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 [RequireComponent(typeof(Text))]
 public class UIText : MonoBehaviour
 {
-    Text uiText;
-    void Start()
-    {
-        uiText = GetComponent<Text>();
-    }
-
+    Text uiText1;
+    Text uiText2;
+    Text uiText3;
     // Update is called once per frame
     void Update()
     {
         if(GameObject.Find("XR Rig").GetComponent<PlayerInput>().isLeft){
-            uiText.text = string.Format("왼쪽 회전 적용");
+            uiText1.GetComponent<Text>();
+            uiText1.text = string.Format("왼쪽 회전 적용");
+            uiText1.DOFade(0, 2);
+            uiText2.DOFade(1, 0.1f);
+            uiText3.DOFade(1, 0.1f);
         }
         else if(GameObject.Find("XR Rig").GetComponent<PlayerInput>().isRight)
         {
-            uiText.text = string.Format("오른쪽 회전 적용");
+            uiText2.GetComponent<Text>();
+            uiText2.text = string.Format("오른쪽 회전 적용");
+            uiText1.DOFade(1, 0.1f);
+            uiText2.DOFade(0, 2);
+            uiText3.DOFade(1, 0.1f);
         }
         else
         {
-            uiText.text = string.Format("무회전");
+            uiText3.GetComponent<Text>();
+            uiText3.text = string.Format("무회전");
+            uiText1.DOFade(1, 0.1f);
+            uiText2.DOFade(1, 0.1f);
+            uiText3.DOFade(0, 2);
         }
     }
 }
