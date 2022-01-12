a2138d4ebf7baac10918b38e026da40a83054d95
unknown <9503chl@naver.com>
2022-01-12 18:55:36 +0900

    '22.01.12'

--- Assets/Scripts/XRInteraction.cs
+++ Assets/Scripts/XRInteraction.cs
@@ -7,37 +7,41 @@ using UnityEngine.UI;
 public class XRInteraction : MonoBehaviour
 {
     [SerializeField] GameObject Panel1;
-    [SerializeField] Image image;
+    [SerializeField] GameObject Box1;
+    [SerializeField] Material material1;
     bool Active1 = false;
     bool isNext = false;
     bool isblack = false;
+    bool isEnd = false;
     float color = 0;
+    int count = 0;
     private void OnCollisionEnter(Collision other)
     {
-        if(other.transform.tag == "Ball")
+        if (other.transform.tag == "Ball")
         {
-            GameObject.Find("Images").SetActive(true);
-            isNext = true;
-            image.color = new Color(image.color.r, image.color.g, image.color.b, color);
             Destroy(other.gameObject);
+            isEnd = true;
+            isNext = true;
+            material1.color = new Color(material1.color.r, material1.color.g, material1.color.b, color);
             Invoke("Loading", 2.0f);
         }
         if (other.transform.name == "Shoes")
         {
-            GameObject.Find("Images").SetActive(true);
+            Destroy(other.gameObject);
+            isEnd = true;
             isNext = true;
-            if(!isblack)
+            if (!isblack)
             {
-                image.color = new Color(0, 0, 0, 0);
+                material1.color = new Color(0, 0, 0, 0);
                 isblack = true;
             }
-            image.color = new Color(image.color.r, image.color.g, image.color.b, color);
-            Destroy(other.gameObject);
+            material1.color = new Color(material1.color.r, material1.color.g, material1.color.b, color);
             Invoke("Quiting", 2.0f);
         }
         if (other.transform.name == "Pinp")
         {
             Destroy(other.gameObject);
+            count++;
             if (!Active1)
             {
                 Panel1.SetActive(true);
@@ -50,11 +54,16 @@ public class XRInteraction : MonoBehaviour
             }
         }
     }
-    private void Update()
+    private void FixedUpdate()
     {
         color = Mathf.Clamp(color, 0, 1);
-        image.transform.position = gameObject.transform.position + new Vector3(0, 0, 0.5f);
         if (isNext) color += Time.deltaTime;
+        if (isEnd) Box1.transform.position = gameObject.transform.position + new Vector3(0, 0, 0.5f);
+        if (count == 10)
+        {
+            GameObject.Find("Magnet").GetComponent<MagnetMove>().count = 3;
+            count = 0;
+        }
     }
     void Loading()
     {
